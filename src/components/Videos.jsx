d2114584015c7d4a9676ce445f8dc4a30e568521
argalusmp <argalus03@gmail.com>
2023-08-16 20:35:08 +0700

    ChannelCard and fixing videos.jsx

--- src/components/Videos.jsx
+++ src/components/Videos.jsx
@@ -5,18 +5,17 @@ import { ChannelCard, Loader, VideoCard } from "./";
 
 const Videos = ({ videos}) => {
   console.log(videos);
-  // if(!videos?.length) return <Loader />;
+  if(!videos?.length) return <Loader />;
   
   return (
-    <div>Videos</div>
-    // <Stack direction={direction || "row"} flexWrap="wrap" justifyContent="start" alignItems="start" gap={2}>
-    //   {videos.map((item, idx) => (
-    //     <Box key={idx}>
-    //       {item.id.videoId && <VideoCard video={item} /> }
-    //       {item.id.channelId && <ChannelCard channelDetail={item} />}
-    //     </Box>
-    //   ))}
-    // </Stack>
+    <Stack direction="row" flexWrap="wrap" justifyContent="start" alignItems="start" gap={2}>
+      {videos.map((item, idx) => (
+        <Box key={idx}>
+          {item.id.videoId && <VideoCard video={item} /> }
+          {item.id.channelId && <ChannelCard channelDetail={item} />}
+        </Box>
+      ))}
+    </Stack>
   );
 }
 
